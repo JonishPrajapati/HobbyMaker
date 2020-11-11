ee83f13de60a7c86253eb58d8b088a808d867777
hsinoj1996 <hsinojjonish@gmail.com>
2020-11-11 02:12:57 -0800

    first hobby maker commit

---
+++ HobbyMaker/HobbyMaker.Application.DataAccess/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HobbyMaker.Application.DataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ HobbyMaker/HobbyMaker.Application.Service/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HobbyMaker.Application.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
